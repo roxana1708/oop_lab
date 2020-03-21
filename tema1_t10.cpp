abc21f1959aea0a55849db0ab702739f954911da
roxana1708 <35289939+roxana1708@users.noreply.github.com>
2020-03-21 18:01:31 +0200

    Update and rename tema1_t10 to tema1_t10.cpp

--- tema1_t10.cpp
+++ tema1_t10.cpp
@@ -6,8 +6,7 @@ class Nod
     Nod *pre, *urm;
 
 public:
-    Nod();
-    Nod(int info, Nod *pre, Nod *urm);
+    Nod(int info = 0, Nod *pre = nullptr, Nod *urm = nullptr);
     ~Nod();
 
     int getInfo(){return info;}
@@ -20,12 +19,7 @@ public:
     friend class Lista_dublu_inlantuita;
 };
 
-Nod::Nod() {
-    info = 0;
-    pre = urm = nullptr;
-}
-
-Nod::Nod(int info, Nod *pre = nullptr, Nod *urm = nullptr) {
+Nod::Nod(int info, Nod *pre, Nod *urm) {
     this->info = info;
     this->pre = pre;
     this->urm = urm;
@@ -58,16 +52,15 @@ Info::Info(int val, int poz) {
     this->poz = poz;
 }
 
-
-
 class Lista_dublu_inlantuita
 {
     Nod *prim, *ultim;
 
 public:
     Lista_dublu_inlantuita();
+    Lista_dublu_inlantuita(const Lista_dublu_inlantuita &l);
     ~Lista_dublu_inlantuita();
-    void adauga_element(Nod nod, int poz);
+    void adauga_element(Info info);
     void sterge_element(int poz);
     friend void operator>>(Lista_dublu_inlantuita& list, Info info_nod);
     friend std::ostream& operator<<(std::ostream& out, Lista_dublu_inlantuita& list);
@@ -78,6 +71,10 @@ Lista_dublu_inlantuita::Lista_dublu_inlantuita() {
     prim = ultim = nullptr;
 }
 
+Lista_dublu_inlantuita::Lista_dublu_inlantuita(const Lista_dublu_inlantuita &l) {
+    prim = l.prim;
+    ultim = l.ultim;
+}
 
 Lista_dublu_inlantuita::~Lista_dublu_inlantuita() {
     Nod *curent = prim;
@@ -92,42 +89,46 @@ Lista_dublu_inlantuita::~Lista_dublu_inlantuita() {
     prim = ultim = nullptr;
 }
 
-void Lista_dublu_inlantuita::adauga_element(Nod nod, int poz) {
+void Lista_dublu_inlantuita::adauga_element(Info info) {
     Nod *prev, *curent;
     Nod *p;
     int i;
-    p = new Nod(nod.getInfo());
+    p = new Nod(info.getVal());
     if(prim == nullptr){
         prim = ultim = p;
+    }else if (info.getPoz() == 1){
+        prim->pre = p;
+        p->urm = prim;
+        prim = p;
     }else{
-        curent = prim;
-        for(i = 1; i < poz && curent != nullptr; i++){
-            prev = curent;
-            curent = curent->urm;
-        }
-        if(curent != nullptr){
-            prev->urm = p;
-            p->pre = prev;
-            p->urm = curent;
-            curent->pre = p;
-        }else{
-            ultim->urm = p;
-            p->pre = ultim;
-            p->urm = nullptr;
-            ultim = p;
-        }
+            curent = prim;
+            for(i = 1; i < info.getPoz() && curent != nullptr; i++){
+                prev = curent;
+                curent = curent->urm;
+            }
+            if(curent != nullptr){
+                prev->urm = p;
+                p->pre = prev;
+                p->urm = curent;
+                curent->pre = p;
+            }else{
+                ultim->urm = p;
+                p->pre = ultim;
+                p->urm = nullptr;
+                ultim = p;
+            }
 
-    }
+        }
 }
 
 void Lista_dublu_inlantuita::sterge_element(int poz) {
     Nod *curent, *temp;
-
+    curent = this->prim;
     if(poz == 1){
         this->prim = this->prim->urm;
         this->prim->pre = nullptr;
     }else {
-        curent = this->prim;
+        //curent = this->prim;
         for (int i = 1; i < poz; i++) {
             temp = curent;
             curent = curent->urm;
@@ -140,20 +141,23 @@ void Lista_dublu_inlantuita::sterge_element(int poz) {
             this->ultim = temp;
         }
     }
+    delete curent;
 }
 
 void operator>>(Lista_dublu_inlantuita& list, Info info_nod) {
-    list.adauga_element(Nod(info_nod.getVal()), info_nod.getPoz());
+    list.adauga_element(info_nod);
 }
 
 std::ostream& operator<<(std::ostream& out, Lista_dublu_inlantuita& list) {
     Nod *curent = list.prim;
+    std::cout << "> ";
     while(curent){
         std::cout << curent->getInfo() << " ";
         curent = curent->getUrm();
     }
     std::cout<<std::endl;
     curent = list.ultim;
+    std::cout << "< ";
     while(curent){
         std::cout << curent->getInfo() << " ";
         curent = curent->getPre();
@@ -162,29 +166,20 @@ std::ostream& operator<<(std::ostream& out, Lista_dublu_inlantuita& list) {
 }
 
 Lista_dublu_inlantuita& operator+(Lista_dublu_inlantuita &list1, Lista_dublu_inlantuita &list2) {
-    static Lista_dublu_inlantuita list3;
+    static Lista_dublu_inlantuita list3 = list1;
     Nod *curent;
-    Info temp{list1.prim->getInfo(), 1};
-    list3>>temp;
+    int i = 1;
 
-    int i = 2;
-    curent = list1.prim->getUrm();
-    while(curent){
-        temp.setVal(curent->getInfo());
-        temp.setPoz(i);
-        list3>>temp;
-        curent = curent->getUrm();
-        i++;
-    }
+    curent = list3.prim;
+    while(curent){i++; curent = curent->getUrm();}
 
     curent = list2.prim;
     while(curent){
-        temp.setVal(curent->getInfo());
-        temp.setPoz(i);
-        list3>>temp;
+        list3>>Info {curent->getInfo(), i};
         curent = curent->getUrm();
         i++;
     }
+
     return list3;
 }
 
@@ -192,20 +187,20 @@ int main() {
 
     Lista_dublu_inlantuita l1;
     Info i{7, 7}, j{8, 8};
-    l1.adauga_element(Nod(1), 1);
-    l1.adauga_element(Nod(2), 2);
-    l1.adauga_element(Nod(3), 2);
-    l1.adauga_element(Nod(4), 4);
-    l1.adauga_element(Nod(5), 5);
-    l1.adauga_element(Nod(6), 6);
-    l1.sterge_element(6);
+    l1.adauga_element(Info {1, 1});
+    l1.adauga_element(Info {2, 2});
+    l1.adauga_element(Info {3, 3});
+    l1.adauga_element(Info {4, 4});
+    l1.adauga_element(Info {5, 5});
+    l1.adauga_element(Info {6, 6});
+    l1.sterge_element(5);
     l1 >> i;
     l1 >> j;
     std::cout << l1 << std::endl;
     Lista_dublu_inlantuita l2;
-    l2.adauga_element(Nod(4), 1);
-    l2.adauga_element(Nod(5), 2);
-    l2.adauga_element(Nod(6), 3);
+    l2.adauga_element({4, 1});
+    l2.adauga_element({5, 1});
+    l2.adauga_element({6, 1});
     std::cout << l2;
     std::cout << std::endl;
     Lista_dublu_inlantuita l3 = l1+l2;
